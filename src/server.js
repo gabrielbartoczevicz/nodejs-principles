a56880fcf7a6d42dd18f708b4c96c42b68ccf80a
Gabriel Bartoczevicz <gabriel.bartoczevicz@gmail.com>
2021-03-16 18:48:08 -0300

    Adicionando o Nodemon na aplicação

--- src/server.js
+++ src/server.js
@@ -2,6 +2,6 @@ const express = require('express')
 
 const app = express()
 
-app.get('/', (request, response) => response.json({ message: 'Henlo World!' }))
+app.get('/', (request, response) => response.json({ message: 'Henlo Ignite!' }))
 
 app.listen(3333, () => console.log('Server started at :3333'))

2e01b61d777df9a3c672e21b62f5816bcb932bbe
Gabriel Bartoczevicz <gabriel.bartoczevicz@gmail.com>
2021-03-17 13:38:19 -0300

    Utilizando os métodos HTTP

--- src/server.js
+++ src/server.js
@@ -1,7 +1,25 @@
-const express = require('express')
+const express = require('express');
 
-const app = express()
+const app = express();
 
-app.get('/', (request, response) => response.json({ message: 'Henlo Ignite!' }))
+app.get('/courses', (_, response) =>
+  response.json(['Curso 1', 'Curso 2', 'Curso 3'])
+);
 
-app.listen(3333, () => console.log('Server started at :3333'))
+app.post('/courses', (_, response) =>
+  response.send(['Curso 1', 'Curso 2', 'Curso 3', 'Curso 4'])
+);
+
+app.put('/courses/:id', (_, response) =>
+  response.send(['Curso 1 a', 'Curso 2', 'Curso 3', 'Curso 4'])
+);
+
+app.patch('/courses/:id', (_, response) =>
+  response.send(['Curso 1 a', 'Curso 2 a', 'Curso 3', 'Curso 4'])
+);
+
+app.delete('/courses/:id', (_, response) =>
+  response.send(['Curso 2', 'Curso 3', 'Curso 4'])
+);
+
+app.listen(3333, () => console.log('Server started at :3333'));

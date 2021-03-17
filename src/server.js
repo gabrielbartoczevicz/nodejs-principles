b5ba7eb8b2402c9755e1a0d8a173d0f96893d3c9
Gabriel Bartoczevicz <gabriel.bartoczevicz@gmail.com>
2021-03-17 18:58:24 -0300

    Tipos de parâmetros

--- src/server.js
+++ src/server.js
@@ -2,17 +2,33 @@ const express = require('express');
 
 const app = express();
 
-app.get('/courses', (_, response) =>
-  response.json(['Curso 1', 'Curso 2', 'Curso 3'])
-);
+app.use(express.json())
 
-app.post('/courses', (_, response) =>
-  response.send(['Curso 1', 'Curso 2', 'Curso 3', 'Curso 4'])
-);
+app.get('/courses', (request, response) => {
+  const { page, limit, order } = request.query;
 
-app.put('/courses/:id', (_, response) =>
-  response.send(['Curso 1 a', 'Curso 2', 'Curso 3', 'Curso 4'])
-);
+  console.log({ page, limit, order });
+
+  return response.json(['Curso 1', 'Curso 2', 'Curso 3']);
+});
+
+app.post('/courses', (request, response) => {
+  const { course } = request.body;
+
+  const courses = ['Curso 1', 'Curso 2', 'Curso 3', 'Curso 4']
+
+  courses.push(course)
+
+  response.send(courses)
+});
+
+app.put('/courses/:id', (request, response) => {
+  const { id } = request.params;
+
+  console.log({ id });
+
+  return response.send(['Curso 1 a', 'Curso 2', 'Curso 3', 'Curso 4']);
+});
 
 app.patch('/courses/:id', (_, response) =>
   response.send(['Curso 1 a', 'Curso 2 a', 'Curso 3', 'Curso 4'])
